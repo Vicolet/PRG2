0124c344f756c2aff949040597e59c9cc7535a7f
Vicolet <victor.nicolet@heig-vd.ch>
2023-03-15 16:45:05 +0100

    ajout d'exo

--- BScode.cpp
+++ BScode.cpp
@@ -1,24 +1,45 @@
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-#define ABS(X) (((X) > 0) ? (X) : (-X))
+#define INT_SIZE sizeof(int) * 8
+
+typedef unsigned short ushort;
+
+int *setBit(unsigned short pos, unsigned short bitValue, int *n);
 
 int main(void) {
-    int n;
-    printf("1. %d\n", ABS(2));
-    printf("2. %d\n", ABS(-2));
-    printf("3. %d\n", ABS('A'));
-    n = -2;
-    printf("4. %d\n", ABS(n + 1));
-    n = -2;
-    printf("5. %d", ABS(n++));
-    printf(" %d\n", n);
-    n = -2;
-    printf("6. %d", ABS(++n));
-    printf(" %d\n", n);
 
-    n = -2;
-    printf("7. %d", abs(++n));
-    printf(" %d\n", n);
-    return EXIT_SUCCESS;
+	int n = 0;
+	printf("n = %d\n");
+	printf("n = %d\n", setBit(0, 1, &n));
+	printf("n = %d\n", setBit(0, 0, &n));
+	printf("n = %d\n", setBit(1, 0, &n));
+
+	for (ushort pos = 0; pos < INT_SIZE; ++pos)
+		setBit(pos, 1, &n);
+	printf("n = %d\n", n);
+
+	int m = -1;
+	printf("m = %d\n", setBit(31, 0, &m));
+
+	return EXIT_SUCCESS;
+}
+
+int *setBit(unsigned short pos, unsigned short bitValue, int *n) {
+	assert(pos < INT_SIZE);
+	assert(bitValue == 0 || bitValue == 1);
+	// Variante 1
+	const int MASK_1 = 1 << pos;
+	const int MASK_2 = bitValue << pos;
+	*n = (*n & MASK_1) | MASK_2;
+/*	{// Variante 2
+		const int MASK = 1 << pos;
+		*n ^= (-bitValue ^ *n) & MASK;
+	}
+	{// Variante 3
+		const int MASK = 1 << pos;
+		*n = bitValue ? *n | MASK : *n & -MASK; // ... mais usage de ?:
+	}*/
+	return n;
 }
