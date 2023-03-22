e29106a0ce79ad3066e89ab828aaa979811f302b
Vicolet <victor.nicolet@heig-vd.ch>
2023-03-22 16:40:04 +0100

    avancé exo

--- BScode.cpp
+++ BScode.cpp
@@ -1,45 +1,17 @@
-#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-#define INT_SIZE sizeof(int) * 8
-
-typedef unsigned short ushort;
-
-int *setBit(unsigned short pos, unsigned short bitValue, int *n);
-
 int main(void) {
-
-	int n = 0;
-	printf("n = %d\n");
-	printf("n = %d\n", setBit(0, 1, &n));
-	printf("n = %d\n", setBit(0, 0, &n));
-	printf("n = %d\n", setBit(1, 0, &n));
-
-	for (ushort pos = 0; pos < INT_SIZE; ++pos)
-		setBit(pos, 1, &n);
-	printf("n = %d\n", n);
-
-	int m = -1;
-	printf("m = %d\n", setBit(31, 0, &m));
+	int n = 127;                    // |127b|
+	double x = 12.3456;             // |0127|
+	printf("R1 : |%-4d|\n", n);     // |7f|
+	printf("R2 : |%04d|\n", n);     // |0177|
+	printf("R3 : |%x|\n", n);       // |12.345600|
+	printf("R4 : |%#o|\n", n);      // |12.35|
+	printf("R5 : |%f|\n", x);       // |1.23e+01|
+	printf("R6 : |%5.2f|\n", x);    // |12.3456|
+	printf("R7 : |%.2e|\n", x);
+	printf("R8 : |%g|\n", x);
 
 	return EXIT_SUCCESS;
-}
-
-int *setBit(unsigned short pos, unsigned short bitValue, int *n) {
-	assert(pos < INT_SIZE);
-	assert(bitValue == 0 || bitValue == 1);
-	// Variante 1
-	const int MASK_1 = 1 << pos;
-	const int MASK_2 = bitValue << pos;
-	*n = (*n & MASK_1) | MASK_2;
-/*	{// Variante 2
-		const int MASK = 1 << pos;
-		*n ^= (-bitValue ^ *n) & MASK;
-	}
-	{// Variante 3
-		const int MASK = 1 << pos;
-		*n = bitValue ? *n | MASK : *n & -MASK; // ... mais usage de ?:
-	}*/
-	return n;
 }
